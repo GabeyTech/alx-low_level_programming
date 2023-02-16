59d09eb6dc9cceca291f777c5d7848641ec723ca
GabeyTech <gabrieltibi56@gmail.com>
2023-02-16 09:49:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,9 @@
-#include <stdio.h>
 #include <unistd.h>
-
-/**
- * main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1. 
+/**                                                   
+ * main - prints a string and standard error
+ * Return: 1                                          
  */
-
-int main(void)
-
-{
-  
+int main(void)                                       {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
