a28249bd45366115a5684a3a017a30e21fef394e
GabeyTech <gabrieltibi56@gmail.com>
2023-02-16 12:38:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - prints a string and standard error
- * Return: 1
+ * main - Entry point
+ * Return: 1.
  */
-int main(void)                                       
-{                                                    
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
