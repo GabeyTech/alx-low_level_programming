120dfb79647de31d3f73db098a30a3e8e0f59d98
GabeyTech <gabrieltibi56@gmail.com>
2023-02-21 11:09:03 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
-#include <unistd.h>
 #include "main.h"
-
+#include <unistd.h>
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char word[9] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
-	_putchar('\n');
-
-	return (0);
+return (write(1, &c, 1));
 }
