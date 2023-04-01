8e6941694ef932c6f930099f8aa5d3542a0a4d99
GabeyTech <gabrieltibi56@gmail.com>
2023-03-31 18:53:59 -0700

    100-main.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+/* Using constructors in C */
+void main_constructor(void) __attribute__((constructor));
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main_constructor - A constructor function that prints a message
+ * before main runs
+ * Return: Nothing
  */
-int main(void)
+void main_constructor(void)
 {
-printf("(A tortoise, having pretty good sense of a hare's nature,.)\n");
-return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
+ 
