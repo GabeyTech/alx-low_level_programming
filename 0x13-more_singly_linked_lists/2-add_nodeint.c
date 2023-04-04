e6cf714c30b2d4581c46254177ef25b91c1a9be4
GabeyTech <gabrieltibi56@gmail.com>
2023-04-04 08:42:28 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,8 +15,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
